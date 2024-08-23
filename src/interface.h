0b5d4263ee28bae955cb0118064d3d90c02575d0
Martin Jaime Flores Jr <martinflores@renci.org>
2024-08-23 13:41:40 -0500

    Simplify interface

--- src/interface.h
+++ src/interface.h
@@ -1,17 +1,10 @@
 #ifndef USER_MAPPER_INTERFACE
 #define USER_MAPPER_INTERFACE
 
-struct UserMgr;
-
-struct UserPrf {
-  char* irods_username;
-  char* attributes;
-};
-
 extern "C" {
-  UserMgr* user_manager_init(const char* _args);
-  UserPrf* user_manager_match(UserMgr* _manager, const char* _param);
-  void user_manager_close(UserMgr* _manager);
+  void* user_manager_init(const char* _args);
+  char* user_manager_match(void* _manager, const char* _param);
+  void user_manager_close(void* _manager);
 }
 
 #endif
